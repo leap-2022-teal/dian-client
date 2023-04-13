df04b918a68102c566d6f1f8d40bb07884417872
XvcleeD <Xvclee255@gmail.com>
2023-04-13 18:34:15 +0800

    map key bug

--- components/products.tsx
+++ components/products.tsx
@@ -14,7 +14,7 @@ export function Products(props: any) {
     <>
       {products?.map((product: any) => {
         return (
-          <div className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300 ease-in-out w-64 mb-5">
+          <div key={product._id} className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300 ease-in-out w-64 mb-5">
             <img className="w-full object-cover" src={product.imageUrl} alt="Product Image" />
             <div className="p-3">
               <h3 className="text-md font-semibold text-gray-800 mb-1">{product.title}</h3>
