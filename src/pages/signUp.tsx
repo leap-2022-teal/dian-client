90a32aaeb5de836a3cce21525cb50f6084c7b516
oyu <teal.2022.oyuerdene@gmail.com>
2023-04-21 18:00:29 +0800

    signUp final

--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -4,6 +4,7 @@ import { fetcherPost } from '../../utils/fetcher';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai';
 
 export default function UserSignUp() {
   const [email, setEmail] = useState('');
@@ -13,34 +14,20 @@ export default function UserSignUp() {
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [error, setError] = useState(false);
   const [isCheck, setIsCheck] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
   function signUp(event: any) {
     event.preventDefault();
-    if (isCheck) {
-      fetcherPost('user/register', { email, password, confirmPassword }).then((res) => {
-        const { status } = res;
-        if (status === 200) {
-          toast('Амжилттай', {
-            position: 'top-right',
-            theme: 'dark',
-            type: 'success',
-            autoClose: 3000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-          });
-          setEmail('');
-          setPassword('');
-          setConfirmPassword('');
-          setIsCheck(false);
-        } else if (status === 400) {
-          return res.json().then((data) => {
-            toast(data.message, {
+    if (password.length >= 8) {
+      setPasswordError(false);
+      if (isCheck) {
+        fetcherPost('user/register', { email, password, confirmPassword }).then((res) => {
+          const { status } = res;
+          if (status === 200) {
+            toast('Амжилттай', {
               position: 'top-right',
-              type: 'error',
               theme: 'dark',
+              type: 'success',
               autoClose: 3000,
               hideProgressBar: false,
               closeOnClick: true,
@@ -48,11 +35,31 @@ export default function UserSignUp() {
               draggable: true,
               progress: undefined,
             });
-          });
-        }
-      });
+            setEmail('');
+            setPassword('');
+            setConfirmPassword('');
+            setIsCheck(false);
+          } else if (status === 400) {
+            return res.json().then((data) => {
+              toast(data.message, {
+                position: 'top-right',
+                type: 'error',
+                theme: 'dark',
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+              });
+            });
+          }
+        });
+      } else {
+        setError(true);
+      }
     } else {
-      setError(true);
+      setPasswordError(true);
     }
   }
 
@@ -87,41 +94,28 @@ export default function UserSignUp() {
                     onChange={(e) => {
                       setEmail(e.target.value);
                     }}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
                 </div>
-                <div>
+                <div className="relative">
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                   <input
                     type={showPassword ? 'text' : 'password'}
                     value={password}
                     onChange={(e) => {
-                      setPassword(e.target.value);
+                      setPassword(e.target.value), setPasswordError(false);
                     }}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
-                  <button type="button" onClick={handlePasswordVisibility}>
-                    {showPassword ? (
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black dark:text-white">
-                        <path
-                          strokeLinejoin="round"
-                          strokeLinecap="round"
-                          d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                        />
-                      </svg>
-                    ) : (
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black dark:text-white">
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                        />
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                      </svg>
-                    )}
-                  </button>
+                  <div className="absolute top-10 right-4">
+                    <button type="button" className="text-gray-600 dark:text-white" onClick={handlePasswordVisibility}>
+                      {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+                    </button>
+                  </div>
+                  {passwordError && <p className="text-red-500 text-sm mt-2">Password must be at least 8 characters long.</p>}
                 </div>
-                <div>
+
+                <div className="relative">
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
                   <input
                     type={showConfirmPassword ? 'text' : 'password'}
@@ -129,28 +123,13 @@ export default function UserSignUp() {
                     onChange={(e) => {
                       setConfirmPassword(e.target.value);
                     }}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
-                  <button type="button" onClick={handleConfirmPasswordVisibility}>
-                    {showConfirmPassword ? (
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black dark:text-white">
-                        <path
-                          strokeLinejoin="round"
-                          strokeLinecap="round"
-                          d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                        />
-                      </svg>
-                    ) : (
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-black dark:text-white">
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                        />
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                      </svg>
-                    )}
-                  </button>
+                  <div className="absolute top-10 right-4">
+                    <button type="button" className="text-gray-600 dark:text-white" onClick={handleConfirmPasswordVisibility}>
+                      {showConfirmPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+                    </button>
+                  </div>
                 </div>
                 {!error ? (
                   <div className="flex items-start">
