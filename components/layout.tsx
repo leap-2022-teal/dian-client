19bb9efd4213837f2ac54c0ebc1771ffb33d618f
oyu <teal.2022.oyuerdene@gmail.com>
2023-05-04 15:09:12 +0800

    login modal bg blur

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,6 @@ export default function Layout({ children }: any) {
     <div className="container mx-auto">
       <Header />
       <CategoryFilter />
-      {/* <div className=" grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4"> {children}</div> */}
       {children}
     </div>
   );
