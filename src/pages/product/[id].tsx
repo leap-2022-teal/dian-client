cd989a03da8e5755ab8fbf6f55305a72f7b22685
oyu <teal.2022.oyuerdene@gmail.com>
2023-05-19 12:44:52 +0800

    quantity

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -6,6 +6,7 @@ import numeral from 'numeral';
 
 export default function SingleProduct() {
   const [product, setProducts] = useState<any>();
+  const [quantity, setQuantity] = useState(1);
   const router = useRouter();
   const { id } = router.query;
   useEffect(() => {
@@ -14,15 +15,23 @@ export default function SingleProduct() {
     }
   }, [router]);
 
+  function handleMinus() {
+    setQuantity(+quantity - 1);
+  }
+
+  function handleAdd() {
+    setQuantity(+quantity + 1);
+  }
+
   return (
     <>
       <Header />
       <div className="container px-5 py-24 mx-auto cursor-pointer">
         <div className="lg:w-4/5 mx-auto flex flex-wrap">
-          <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded cursor-pointer" src={product && product[0].imageUrl} />
+          <img alt={product && product[0]?.title} className="lg:w-auto w-[40vh] lg:h-[70vh] h-auto object-cover object-center rounded cursor-pointer" src={product && product[0].imageUrl} />
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 cursor-pointer">
             <h2 className="text-sm title-font text-gray-500 tracking-widest cursor-pointer">ON SALE</h2>
-            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1 cursor-pointer">{product && product[0]?.title}</h1>
+            <h1 className="text-gray-900 text-2xl title-font font-medium mb-1 cursor-pointer">{product && product[0]?.title}</h1>
             <div className="flex mb-4">
               <span className="flex items-center">
                 <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
@@ -56,21 +65,38 @@ export default function SingleProduct() {
                 <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
                 <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
               </div>
-              <div className="flex ml-6 items-center">
-                <span className="mr-3">Size</span>
-                <div className="relative">
-                  <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                    <option>SM</option>
-                    <option>M</option>
-                    <option>L</option>
-                    <option>XL</option>
-                  </select>
-                  <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4" viewBox="0 0 24 24">
-                      <path d="M6 9l6 6 6-6"></path>
-                    </svg>
-                  </span>
+              <div className="flex items-center space-x-3 ml-[3rem]">
+                <button
+                  onClick={handleMinus}
+                  className="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  type="button"
+                >
+                  <span className="sr-only">Quantity button</span>
+                  <svg className="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                  </svg>
+                </button>
+
+                <div>
+                  <input
+                    value={quantity}
+                    onChange={(e: any) => setQuantity(e.target.value)}
+                    id="first_product"
+                    className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
                 </div>
+
+                <button
+                  onClick={handleAdd}
+                  className="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  type="button"
+                >
+                  <span className="sr-only">Quantity button</span>
+                  <svg className="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
+                  </svg>
+                </button>
               </div>
             </div>
             <div className="flex">
