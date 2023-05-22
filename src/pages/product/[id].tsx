80357a6d6c7a9f52472a2c33ec789283e4af2fe4
oyu <teal.2022.oyuerdene@gmail.com>
2023-05-22 13:39:46 +0800

    footer,logo

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -71,7 +71,7 @@ export default function SingleProduct() {
                 >
                   <span className="sr-only">Quantity button</span>
                   <svg className="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                    <path fillRule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
                   </svg>
                 </button>
 
@@ -92,7 +92,7 @@ export default function SingleProduct() {
                 >
                   <span className="sr-only">Quantity button</span>
                   <svg className="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
+                    <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd"></path>
                   </svg>
                 </button>
               </div>
